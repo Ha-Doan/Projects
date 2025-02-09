87e281947e12d1af217029100c92404d9efcda48
Ha Doan <doanthanhha@gmail.com>
2025-02-10 00:21:39 +0100

    Added UI components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import './App.css';
+import Col from 'react-bootstrap/Col';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import { ButtonGroup } from 'react-bootstrap';
 
 function App() {
-  const logo = require("./logo.svg") as string;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Message App</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="message">
+          <Form.Label>
+            Message
+          </Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+        <ButtonGroup className="mb-3">
+        <Button variant="success">Send message</Button>
+        </ButtonGroup>
+        <br />
+        <ButtonGroup className="mb-3">
+          <Button variant="info">Get message</Button>
+        </ButtonGroup>
+      </Form>
+    </Container>
   );
 }
 
